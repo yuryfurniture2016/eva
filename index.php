ca2eb61b2c034468c402c4bb0957a74a9c706975
yuryfurniture <90131788+yuryfurniture2016@users.noreply.github.com>
2023-02-22 20:36:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?PHP
-$ch = curl_init('https://vadims-marvelous-site-771808.webflow.io/');
+$ch = curl_init('https://vadims-marvelous-site-771808.webflow.io');
 curl_exec($ch);
 curl_close($ch);
 ?>
